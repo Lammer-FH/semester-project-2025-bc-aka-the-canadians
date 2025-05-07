5c63e731b171f497fed65a66da9753dc2dc11b18
kaplanilhan <113658728+kaplanilhan@users.noreply.github.com>
2025-05-07 18:05:20 +0200

    add .gitignore

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <ion-page>
+    <ion-content>
+      <h1>Campus Lost & Found</h1>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup>
+import { IonPage, IonContent } from '@ionic/vue'
+</script>
+
+<style scoped>
+.home {
+  padding: 20px;
+}
+</style> 

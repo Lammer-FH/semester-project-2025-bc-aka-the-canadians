8a453823ff99b65b2d9f0a8a7ed82a651c5ce02f
Plöchl Alexander <a.ploechl@palfinger.com>
2025-05-29 14:05:05 +0200

    update FE models to fit BE models

--- frontend/src/models/location.ts
+++ frontend/src/models/location.ts
@@ -1,28 +1,30 @@
 import { Item } from './item';
 
 export interface Location {
-  id: number;
-  name: string;
-  description: string;
-  building: string;
-  floor: string;
-  room: string;
-  createdAt: string;
-  updatedAt: string;
+	id: number;
+	name: string;
+	description: string;
+	building: string;
+	floor: string;
+	roomNumber: string;
+	latitude?: number;
+	longitude?: number;
+	createdAt: string;
+	updatedAt: string;
 }
 
 export interface LocationCreateData {
-  name: string;
-  description: string;
-  building: string;
-  floor: string;
-  room: string;
+	name: string;
+	description: string;
+	building: string;
+	floor: string;
+	room: string;
 }
 
 export interface LocationUpdateData {
-  name?: string;
-  description?: string;
-  building?: string;
-  floor?: string;
-  room?: string;
-} 
+	name?: string;
+	description?: string;
+	building?: string;
+	floor?: string;
+	room?: string;
+}
